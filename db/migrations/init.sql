8ff18497c7e747c25953b4f877e51039807231b2
kwon young jin <kwon120@hanyang.ac.kr>
2025-02-18 16:56:23 +0900

    fix : sql 테이블 수정
    (2월 18일 토의한 내용에 따른 테이블 추가 및 일부 필드 수정)

--- db/migrations/init.sql
+++ db/migrations/init.sql
@@ -2,63 +2,77 @@ CREATE DATABASE IF NOT EXISTS unwritten_db;
 USE unwritten_db;
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id VARCHAR(50) NOT NULL PRIMARY KEY,
-    nickname VARCHAR(255) NOT NULL
+    user_id VARCHAR(50) NOT NULL PRIMARY KEY, -- 유저 고유 아이디
+    nickname VARCHAR(50) NOT NULL, -- 유저 별명
+    access_level INT NOT NULL -- 접근 가능성
 );
 
 CREATE TABLE IF NOT EXISTS user_quest (
-    user_id VARCHAR(50) NOT NULL,
-    quest_id VARCHAR(255) NOT NULL,
-    progress FLOAT NOT NULL, -- 진척도
+    user_id VARCHAR(50) NOT NULL, -- 유저 고유 아이디
+    quest_id INT NOT NULL, -- 퀘스트 아이디
+    progress INT NOT NULL, -- 진척도
     UNIQUE(user_id, quest_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS user_achievement (
-    user_id VARCHAR(50) NOT NULL,
-    challenge_name VARCHAR(255) NOT NULL,
-    complete_time TIMESTAMP NOT NULL,
-    progress FLOAT NOT NULL,
-    ranking BIGINT, -- 달성 등수
-    UNIQUE(user_id, challenge_name),
+    user_id VARCHAR(50) NOT NULL, -- 유저 고유 아이디
+    achievement_id INT NOT NULL, -- 도전과제 아이디디
+    complete_time TIMESTAMP NOT NULL, -- 달성 시간
+    progress INT NOT NULL, -- 진척도
+    ranking INT, -- 달성 등수
+    UNIQUE(user_id, achievement_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS user_inventory (
-    user_id VARCHAR(50) NOT NULL,
-    item_name VARCHAR(255) NOT NULL,
-    count BIGINT NOT NULL, -- 만약 가질 수 있는 아이템 개수에 제한을 줄 경우 TINYINT (0 ~ 255) 도 고려해볼만할 듯
-    is_clear TINYINT(1) NOT NULL, -- 1 : 달성, 0 : 미달성
-    UNIQUE(user_id, item_name),
+    user_id VARCHAR(50) NOT NULL, -- 유저 고유 아이디
+    item_id INT NOT NULL, -- 아티팩트 아이디디
+    count INT NOT NULL, -- 보유 아티팩트 개수
+    is_collected TINYINT(1) NOT NULL, -- 보유 달성 여부
+    UNIQUE(user_id, item_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS item_instance (
-    item_id VARCHAR(50) NOT NULL PRIMARY KEY, -- 아이템 고유 아이디
-    item_name VARCHAR(255) NOT NULL,
-    user_id VARCHAR(50) NOT NULL,
-    current_state TINYINT(1) NOT NULL, -- 1 : 활성화, 0 : 비활성화
+    item_id INT AUTO_INCREMENT PRIMARY KEY, -- 아티팩트discussion 고유 아이디
+    item_name VARCHAR(255) NOT NULL, -- 아티팩트 이름
+    user_id VARCHAR(50) NOT NULL, -- 유저 고유 아이디
+    current_state TINYINT(1) NOT NULL, -- 현재 아티팩트 상태
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS achievement (
-    challenge_name VARCHAR(255) NOT NULL PRIMARY KEY,
+    achievement_id INT NOT NULL PRIMARY KEY, -- 도전과제 아이디
+    achievement_name VARCHAR(255) NOT NULL, -- 도전과제 제목
     grade VARCHAR(50), -- 도전과제 정보 1 (등급)
     condition_information VARCHAR(255), -- 도전과제 정보 2 (달성조건)
-    content VARCHAR(255), -- 도전과제 정보 3 (내용)
+    reward VARCHAR(50), -- 도전과제 정보 3 (보상)
+    content VARCHAR(255), -- 도전과제 정보 4 (내용)
     clear_count BIGINT NOT NULL -- 달성자 수
 );
 
 CREATE TABLE IF NOT EXISTS item (
-    item_id BIGINT AUTO_INCREMENT PRIMARY KEY, 
-    item_name VARCHAR(255) NOT NULL,
-    grade VARCHAR(50), -- 아이템 정보 1(등급)
-    effect VARCHAR(255), -- 아이템 정보 2(효과)
-    condition_information VARCHAR(255), -- 아이템 정보 3(획득 조건)
-    content VARCHAR(255) -- 아이템 정보 4(내용)
+    item_id INT NOT NULL PRIMARY KEY, -- 아티팩트 고유 아이디
+    item_name VARCHAR(255) NOT NULL, -- 아티팩트 이름
+    grade VARCHAR(50) NOT NULL, -- 아티팩트 정보 1(등급)
+    effect VARCHAR(255), -- 아티팩트 정보 2(효과)
+    kind_of_effect VARCHAR(50), -- 아티팩트 정보 3(효과 종류)
+    can_selling VARCHAR(15) NOT NULL, -- 아티팩트 정보 4(판매 가능 여부)
+    condition_information VARCHAR(255), -- 아티팩트 정보 5(획득 조건)
+    content VARCHAR(255) -- 아티팩트 정보 6(내용)
 );
 
 CREATE TABLE IF NOT EXISTS quest_combination (
-    quest_combination_id INT AUTO_INCREMENT PRIMARY KEY,
-    combination VARCHAR(50) NOT NULL
+    quest_combination_id INT AUTO_INCREMENT PRIMARY KEY, -- 퀘스트 조합 아이디
+    combination VARCHAR(50) NOT NULL -- 조합된 요소들
+);
+
+CREATE TABLE IF NOT EXISTS quest (
+    quest_id INT AUTO_INCREMENT PRIMARY KEY, -- 퀘스트 아이디
+    quest_name VARCHAR(255) NOT NULL, -- 퀘스트 이름
+    completed_progress INT NOT NULL, -- 퀘스트 완료 진척도
+    grade VARCHAR(50) NOT NULL, -- 퀘스트 등급
+    reward VARCHAR(50) NOT NULL, -- 퀘스트 보상
+    content VARCHAR(50) NOT NULL -- 퀘스트 내용
 );
