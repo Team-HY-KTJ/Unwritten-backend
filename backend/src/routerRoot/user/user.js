9997e036ac818b941e7881eb6a28c6120dc9af79
kwon young jin <kwon120@hanyang.ac.kr>
2025-02-19 22:41:16 +0900

    fix : user GET 요청 수정
    (1. 반환 json에 닉네임도 함께 반환하게 변경
     2. try, catch 사용으로 에러 처리)

--- backend/src/routerRoot/user/user.js
+++ backend/src/routerRoot/user/user.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import db from '../../db.js';
+import { rejects } from 'assert';
+import { resolve } from 'path';
+import { userInfo } from 'os';
 
 const route = '/user';
 const router = express.Router();
@@ -10,10 +13,10 @@ const { DB_TABLENAME } = process.env;
 
     입력 쿼리 = {
         userId : "00000000",
-        nickname : "별명"
+        nickname : '홍길동'
     }
 
-    반환 쿼리 = {
+    반환 json = {
         userId : "00000000",
         isSuccess : true
     }
@@ -21,7 +24,9 @@ const { DB_TABLENAME } = process.env;
 router.post('', (req, res) => {
     const { userId, nickname } = req.query;
     const response = { userId : userId, isSuccess : false };
-
+    
+    // 닉네임을 따로 설정하지 않은 상태로 쿼리를 보내도 정상작동이 되고 있는 상태 (유저 초기 등록 시 닉네임 기입을 바로 한다면 입력조건 부여 필요)
+    
     db.query('INSERT IGNORE INTO users (user_id, nickname) VALUES (?, ?)', [userId, nickname], (err) => {
         if (err) {
             console.error('데이터 삽입 오류:', err);
@@ -42,28 +47,36 @@ router.post('', (req, res) => {
         userId : "00000000"
     }
 
-    반환 쿼리 = {
+    반환 json = {
         userId : "00000000",
         exists : true,
-        accessLevel : 3
+        accessLevel : 1,
+        nickname : '홍길동'
     }
 */
 router.get('', async (req, res) => {
     const { userId } = req.query;
-    const response = { userId : userId, exists : null, accessLevel : null };
-    const [result] = await new Promise((resolve, reject) => {
-        db.query('SELECT COUNT(*) AS count, access_level FROM users WHERE user_id = ?', [userId], (err, results) => {
-            if(err) reject(err);
-            resolve(results);
+    const response = { userId : userId, exists : null, accessLevel : null, nickname : null };
+    try {
+        const [result] = await new Promise((resolve, reject) => {
+            db.query('SELECT COUNT(*) AS count, access_level, nickname FROM users WHERE user_id = ?', [userId], (err, results) => {
+                if(err) reject(err);
+                resolve(results);
+            });
         });
-    });
-    response.accessLevel = result.access_level;
 
-    if(result.count === 0){
-        response.exists = false;
+        response.accessLevel = result.access_level;
+        response.nickname = result.nickname;
+    
+        if(result.count === 0){
+            response.exists = false;
+        }
+        else {
+            response.exists = true;
+        }
     }
-    else {
-        response.exists = true;
+    catch(e){
+        console.log(e);
     }
     
     console.log(response);
