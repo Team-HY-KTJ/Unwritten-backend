2f5079facfd222852b7547a605c157a8b48eda67
kwon young jin <kwon120@hanyang.ac.kr>
2025-02-19 22:52:40 +0900

    feat : 유저 접근 가능도 수정 API 추가
    (유저 접근 가능도 수정하고 UPDATE 여부 검증 후 JSON 반환하게끔 제작작)

--- backend/src/routerRoot/user/user.js
+++ backend/src/routerRoot/user/user.js
@@ -83,4 +83,52 @@ router.get('', async (req, res) => {
     res.json(response);
 });
 
+/*
+    /user (patch) : 유저 접근 가능도 변경
+
+    입력 쿼리 = {
+        userId = "00000000",
+        accessLevel = 3,
+    }
+
+    반환 json = {
+        userId = "00000000",
+        accessLevel = 3,
+        isSuccess = true
+    }
+*/
+router.patch('', async (req,res) => {
+    const { userId, accessLevel } = req.query;
+    const response = { userId : userId, accessLevel : null, isSuccess : false };
+    
+    try {
+        await new Promise((resolve, reject) => {
+            db.query('UPDATE users SET access_level = ? WHERE user_id = ?', [accessLevel, userId], (err, results) => {
+                if(err) reject(err);
+                resolve(results);
+            });
+        });
+        const [result] = await new Promise((resolve, reject) => {
+            db.query('SELECT access_level FROM users WHERE user_id = ?', [userId], (err, results) => {
+                if(err) reject(err);
+                resolve(results);
+            });
+        });
+        
+        if(accessLevel != result.access_level){
+            console.log(accessLevel, "!==", result.access_level);
+            throw new Error('sql UPDATE ERROR');
+        }
+
+        response.accessLevel = result.access_level;
+        response.isSuccess = true;
+    }
+    catch(e){
+        console.log(e);
+    }
+
+    console.log(response);
+    res.json(response);
+});
+
 export { route, router };
