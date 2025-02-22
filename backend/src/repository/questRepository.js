d165e20861d9c642743fe18484e66cd97dbfe147
Yeobi <yeobi1124@gmail.com>
2025-02-22 14:07:25 +0900

    feat : develop quest repository, test pending
    - quest 부분 repository 개발
    - 테스트는 라우터 연결해야 가능해서 보류

---
+++ backend/src/repository/questRepository.js
@@ -0,0 +1,101 @@
+import db from "../db";
+
+async function createUserQuest( userId, questId )
+{
+    const query = `INSERT INTO user_quest (user_id, quest_id, progress) VALUES (?, ?, 0)`;
+    const data = [ userId, questId ];
+
+    try{
+        const [rows] = await new Promise((resolve, reject) => db.query(query, data, (err, result, fields) => {
+            if(err) reject(err);
+            resolve(result); 
+        }));
+
+        return rows;
+    }
+    catch(err){
+        throw new Error(err);
+    }
+}
+
+async function readUserQuests( userId )
+{
+    const query = `
+        SELECT *
+        FROM user_quest
+        LEFT JOIN quest ON user_quest.quest_id = quest.quest_id
+        WHERE user_id = ?
+    `;
+    const data = [ userId ];
+
+    try{
+        const [rows] = await new Promise((resolve, reject) => db.query(query, data, (err, result, fields) => {
+            if(err) reject(err);
+            resolve(result); 
+        }));
+
+        return rows;
+    }
+    catch(err){
+        throw new Error(err);
+    }
+}
+
+async function readUserQuest( userId, questId )
+{
+    const query = `
+        SELECT *
+        FROM user_quest
+        LEFT JOIN quest ON user_quest.quest_id = quest.quest_id
+        WHERE user_id = ? AND quest_id = ?
+    `;
+    const data = [ userId, questId ];
+
+    try{
+        const [rows] = await new Promise((resolve, reject) => db.query(query, data, (err, result, fields) => {
+            if(err) reject(err);
+            resolve(result);
+        }));
+
+        return rows;
+    }
+    catch(err){
+        throw new Error(err);
+    }
+}
+
+async function updateUserQuest( userId, questId, progressChange ) {
+    const query = `UPDATE user_quest SET progress = progress + ? WHERE user_id = ? AND quest_id = ?`;
+    const data = [ userId, questId, progressChange ];
+
+    try{
+        const [rows] = await new Promise((resolve, reject) => db.query(query, data, (err, result, fields) => {
+            if(err) reject(err);
+            resolve(result);
+        }));
+
+        return rows;
+    }
+    catch(err){
+        throw new Error(err);
+    }
+}
+
+async function deleteUserQuest( userId, questId ) {
+    const query = `DELETE FROM user_quest WHERE user_id = ? AND quest_id = ?`;
+    const data = [ userId, questId ];
+
+    try{
+        const [rows] = await new Promise((resolve, reject) => db.query(query, data, (err, result, fields) => {
+            if(err) reject(err);
+            resolve(result); 
+        }));
+
+        return rows;
+    }
+    catch(err){
+        throw new Error(err);
+    }
+}
+
+export default { readUserQuests, readUserQuest, createUserQuest, deleteUserQuest };
